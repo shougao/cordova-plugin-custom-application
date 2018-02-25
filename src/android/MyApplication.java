92553d19eb088b6075d2fbee3dc05f328b937168
zhangqc <zhangqingcai0815@gmail.com>
2018-02-25 21:50:34 +0800

    添加apk下载逻辑

--- src/android/MyApplication.java
+++ src/android/MyApplication.java
@@ -7,6 +7,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -16,7 +18,6 @@ import android.os.HandlerThread;
 import android.os.Message;
 import android.os.Process;
 import android.util.Log;
-
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -36,24 +37,36 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class MyApplication extends Application implements Handler.Callback {
     public static final String TAG = "MyApplication";
 
-    public static final int HANDLE_MESSAGE_CHECK_VERSION = 100;
-    public static final int HANDLE_MESSAGE_CHECK_VERSION_COMPLETE = 101;
+    // business logic.
+    private static final int HANDLE_MESSAGE_CHECK_VERSION = 100;
+    private static final int HANDLE_MESSAGE_CHECK_VERSION_COMPLETE = 101;
+    private static final int HANDLE_MESSAGE_DOWNLOAD = 102;
+    private static final int HANDLE_MESSAGE_DOWNLOAD_COMPLETE = 103;
 
-    public static final int HANDLE_MESSAGE_DOWNLOAD_SO = 102;
-    public static final int HANDLE_MESSAGE_DOWNLOAD_COMPLETE = 103;
+    // provider query event
+    private static final int HANDLE_MESSAGE_DOWNLOAD_PREGRESS = 200;
+
+    // ui event.
+    private static final int HANDLE_UI_ONPROGRESS = 300;
+    private static final int HANDLE_UI_ONCOMPLETE = 301;
 
     //
 //    private static final int RECHECK_INTERVAL_MS = 1000 * 60 * 10; // 如果没网络，10分钟检查一次，直到有网络为止
     public static final int RECHECK_DELAY_MS = 1000 * 10;// delay cordova issues, todo zqc
 
-
     private Handler mWorkerHandler;
     private HandlerThread mWorkerHandlerThread;
+
+    private DownloadChangeObserver mDownloadObserver;
+    private ScheduledExecutorService scheduledExecutorService;
 
     // ====================network part.
     private static final String ENDPOINT = "https://kylewbanks.com/rest/posts.json";
@@ -64,8 +77,21 @@ public class MyApplication extends Application implements Handler.Callback {
     private DownLoadBroadcast mDownLoadBroadcast;
     private long mDownloadId;
 
+    // update download progress.
+    private Handler mUIHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (HANDLE_UI_ONPROGRESS == msg.what) {
+                float onprogress = (float) msg.obj;
+                // TODO: 2/25/18 progress js display.
+            } else if (HANDLE_UI_ONCOMPLETE == msg.what) {
+                // TODO: 2/25/18  complete is display
+            }
+        }
+    };
 
-    private BroadcastReceiver mWifiReceiver = new BroadcastReceiver() {
+    private BroadcastReceiver mWifiConnetedReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -82,9 +108,7 @@ public class MyApplication extends Application implements Handler.Callback {
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate(), ThreadId= " + Thread.currentThread().getId());
-        // DO SOME STUFF
         super.onCreate();
-
         initInternal();
     }
 
@@ -97,7 +121,7 @@ public class MyApplication extends Application implements Handler.Callback {
 
         mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
-        mWorkerHandlerThread = new HandlerThread("soupdate", Process.THREAD_PRIORITY_BACKGROUND);
+        mWorkerHandlerThread = new HandlerThread(Constant.THREAD_NAME, Process.THREAD_PRIORITY_BACKGROUND);
         mWorkerHandlerThread.start();
         mWorkerHandler = new Handler(mWorkerHandlerThread.getLooper(), this);
         if (!mWorkerHandler.hasMessages(HANDLE_MESSAGE_CHECK_VERSION)) {
@@ -105,12 +129,11 @@ public class MyApplication extends Application implements Handler.Callback {
         }
     }
 
-
     @Override
     public boolean handleMessage(Message message) {
         switch (message.what) {
             case HANDLE_MESSAGE_CHECK_VERSION:
-                if (true || isWifiConnected()) { // todo true is for debug.
+                if (isWifiConnected()) {
                     getRemoteNewVersion();
                 } else {
                     startWifiListen();
@@ -122,33 +145,41 @@ public class MyApplication extends Application implements Handler.Callback {
                 int newVersion = (int) message.obj;
                 if (newVersion > localVersion()) {
                     Log.d(TAG, "start checkupdate, thread id=" + Thread.currentThread().getId());
-                    (new Exception(TAG)).printStackTrace();
-                    mWorkerHandler.sendEmptyMessage(HANDLE_MESSAGE_DOWNLOAD_SO);
+                    mWorkerHandler.sendEmptyMessage(HANDLE_MESSAGE_DOWNLOAD);
                 } else {
                     exitHandlerThread();
                 }
                 break;
 
-            case HANDLE_MESSAGE_DOWNLOAD_SO:
+            case HANDLE_MESSAGE_DOWNLOAD:
                 LogUtil.d(TAG, "start download.");
-                downloadFromServer();
+                downloadFromServer("url");
+                break;
+
+            case HANDLE_MESSAGE_DOWNLOAD_PREGRESS:
+                if ((boolean) message.obj) {
+                    mUIHandler.sendMessage(mUIHandler.obtainMessage(HANDLE_UI_ONCOMPLETE));//提示可以安装。
+                } else {
+                    if (message.arg1 >= 0 && message.arg2 > 0) {//更新进度
+                        mUIHandler.sendMessage(mUIHandler.obtainMessage(HANDLE_UI_ONPROGRESS, message.arg1 / (float) message.arg2));
+                    }
+                }
                 break;
 
             case HANDLE_MESSAGE_DOWNLOAD_COMPLETE:
                 boolean success = (Boolean) message.obj;
                 if (success) {
-                    if (mWifiReceiver != null) {
-                        unregisterReceiver(mWifiReceiver);
-                    }
+                    // TODO: 2/25/18 1. 出发安装从js。 2. 如果没有安装，需要保存md5和安装文件， 用于下起提示用户，省去下载流程
+                    exitHandlerThread();
                 } else {
                     // TODO: 2/22/18 zqc
+                    exitHandlerThread();
                 }
                 break;
 
             default:
                 return true;
         }
-
         return true;
     }
 
@@ -166,17 +197,9 @@ public class MyApplication extends Application implements Handler.Callback {
         Log.d(TAG, "stop all. start checkupdate");
     }
 
-    private void exitHandlerThread() {
-
-        if (mWorkerHandlerThread != null) {
-            mWorkerHandlerThread.quit();
-            mWorkerHandlerThread = null;
-
-            Log.d(TAG, "stop thread, start checkupdate");
-        }
-    }
 
     // TODO: 2/22/18 zqc, get from so.
+
     private int localVersion() {
         return SpUtil.getLocalVersion(getApplicationContext());
     }
@@ -189,8 +212,8 @@ public class MyApplication extends Application implements Handler.Callback {
 
     private void startWifiListen() {
         IntentFilter filter = new IntentFilter();
-        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        registerReceiver(mWifiReceiver, filter);
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(mWifiConnetedReceiver, filter);
     }
 
     /**
@@ -215,22 +238,22 @@ public class MyApplication extends Application implements Handler.Callback {
 
 
         // 添加参数
-        StringRequest customRequest = new StringRequest(Request.Method.GET, ENDPOINT, onPostLoaded, onPostError){
+        StringRequest customRequest = new StringRequest(Request.Method.GET, ENDPOINT, onPostLoaded, onPostError) {
             @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("user","x"/*userAccount.getUsername()*/);
-                params.put("pass","x"/*userAccount.getPassword()*/);
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user", "x"/*userAccount.getUsername()*/);
+                params.put("pass", "x"/*userAccount.getPassword()*/);
                 params.put("comment", "x"/*Uri.encode(comment)*/);
-                params.put("comment_post_ID",String.valueOf(0/*postId*/));
-                params.put("blogId",String.valueOf(0/*blogId*/));
+                params.put("comment_post_ID", String.valueOf(0/*postId*/));
+                params.put("blogId", String.valueOf(0/*blogId*/));
                 return params;
             }
 
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("Content-Type","application/x-www-form-urlencoded");
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
                 return params;
             }
         };
@@ -285,7 +308,7 @@ public class MyApplication extends Application implements Handler.Callback {
                 itemNumbers++;
             }
 
-            int fackedVersionNuber = 10;// TODO: 2/23/18 zqc 
+            int fackedVersionNuber = 10;// TODO: 2/23/18 zqc
             Message msg = mWorkerHandler.obtainMessage(HANDLE_MESSAGE_CHECK_VERSION_COMPLETE, fackedVersionNuber);
             mWorkerHandler.sendMessage(msg);
         }
@@ -298,8 +321,12 @@ public class MyApplication extends Application implements Handler.Callback {
         }
     };
 
-    private void downloadFromServer() {
-        String url = "https://cdn2.jianshu.io/assets/web/nav-logo-4c7bbafe27adc892f3046e6978459bac.png";
+    private void downloadFromServer(String url) {
+        url = "http://www.zhaoshangdai.com/file/android.apk";
+
+        mDownloadObserver = new DownloadChangeObserver();
+        registerContentObserver();
+
         Uri uri = Uri.parse(url);
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
@@ -323,6 +350,12 @@ public class MyApplication extends Application implements Handler.Callback {
         registerReceiver(mDownLoadBroadcast, intentFilter);
     }
 
+    private void unregisterWifiListener() {
+        if (mWifiConnetedReceiver != null) {
+            unregisterReceiver(mWifiConnetedReceiver);
+        }
+    }
+
     /**
      * 注销广播
      */
@@ -333,6 +366,39 @@ public class MyApplication extends Application implements Handler.Callback {
         }
     }
 
+    /**
+     * 注册ContentObserver
+     */
+    private void registerContentObserver() {
+        /** observer download change **/
+        if (mDownloadObserver != null) {
+            getContentResolver().registerContentObserver(Uri.parse("content://downloads/my_downloads"), false, mDownloadObserver);
+        }
+    }
+
+    /**
+     * 注销ContentObserver
+     */
+    private void unregisterContentObserver() {
+        if (mDownloadObserver != null) {
+            getContentResolver().unregisterContentObserver(mDownloadObserver);
+        }
+    }
+
+
+    private void exitHandlerThread() {
+
+        unregisterWifiListener();
+        unregisterBroadcast();
+        unregisterContentObserver();
+
+        if (mWorkerHandlerThread != null) {
+            mWorkerHandlerThread.quit();
+            mWorkerHandlerThread = null;
+
+            Log.d(TAG, "stop thread, start checkupdate");
+        }
+    }
 
     /**
      * 接受下载完成广播
@@ -350,12 +416,71 @@ public class MyApplication extends Application implements Handler.Callback {
                             Log.i(TAG, "广播监听下载完成，APK存储路径为 ：" + downIdUri.getPath());
                             SpUtil.put(Constant.SP_DOWNLOAD_PATH, downIdUri.getPath());
 //                            APPUtil.installApk(context, downIdUri);
-                            // TODO: 2/23/18 change so. zqc
+                            mWorkerHandler.sendMessage(mWorkerHandler.obtainMessage(HANDLE_MESSAGE_DOWNLOAD_COMPLETE, "dummy_apk_path"));
                         }
                     }
                     break;
             }
         }
+    }
+
+
+    private class DownloadChangeObserver extends ContentObserver {
+
+        public DownloadChangeObserver() {
+            super(mWorkerHandler);
+            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        }
+
+        /**
+         * 当所监听的Uri发生改变时，就会回调此方法
+         *
+         * @param selfChange 此值意义不大, 一般情况下该回调值false
+         */
+        @Override
+        public void onChange(boolean selfChange) {
+            scheduledExecutorService.scheduleAtFixedRate(progressRunnable, 0, 1, TimeUnit.SECONDS);
+        }
+    }
+
+    private Runnable progressRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "ui thread id =" + Thread.currentThread().getId());
+            int[] bytesAndStatus = getBytesAndStatus(mDownloadId);
+            mWorkerHandler.sendMessage(mUIHandler.obtainMessage(HANDLE_MESSAGE_DOWNLOAD_PREGRESS, bytesAndStatus[0], bytesAndStatus[1], bytesAndStatus[2] == DownloadManager.STATUS_SUCCESSFUL));
+        }
+    };
+
+
+    /**
+     * 通过query查询下载状态，包括已下载数据大小，总大小，下载状态
+     *
+     * @param downloadId
+     * @return
+     */
+    private int[] getBytesAndStatus(long downloadId) {
+        int[] bytesAndStatus = new int[]{
+                -1, -1, 0
+        };
+        DownloadManager.Query query = new DownloadManager.Query().setFilterById(downloadId);
+        Cursor cursor = null;
+        try {
+            cursor = mDownloadManager.query(query);
+            if (cursor != null && cursor.moveToFirst()) {
+                //已经下载文件大小
+                bytesAndStatus[0] = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                //下载文件的总大小
+                bytesAndStatus[1] = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                //下载状态
+                bytesAndStatus[2] = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return bytesAndStatus;
     }
 
 }
